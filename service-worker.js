6d24094919eeac8bc4c27d56a587ceb359b163db
zhjpwlj <141892783+zhjpwlj@users.noreply.github.com>
2025-08-02 18:15:30 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = 'super-flashcard-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css',
-  '/app.js',
-  '/firebase-config.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+const CACHE_NAME = "flashmy-cache-v1";
+const STATIC_ASSETS = [
+  "./",
+  "./index.html",
+  "./styles/styles.css",
+  "./app/main.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request).then((cached) =>
+      cached || fetch(event.request).catch(() => caches.match("index.html"))
+    )
   );
 });
